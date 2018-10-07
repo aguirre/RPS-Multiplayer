46b848ec8516a73165daedfa0ea9080ec0b71542
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-10-07 02:23:51 -0700

    add chat function and update display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,10 +23,9 @@ var p1 = null;
 var p2 = null;
 var wins1 = 0;
 var wins2 = 0;
-var losses1 = 0;
-var losses2 = 0;
-var playerNum = 0;
-var chatting;
+var loss1 = 0;
+var loss2 = 0;
+var playerNumber = 0;
 
 // Creates area for player to enter their name
 $("#welcomeMessage").html(
@@ -38,10 +37,10 @@ player1.on("value", function(snapshot) {
   if (snapshot.val() !== null) {
     p1 = snapshot.val().player;
     wins1 = snapshot.val().wins;
-    losses1 = snapshot.val().losses;
+    loss1 = snapshot.val().losses;
     $("#playerOneName").html("<h2>" + p1 + "</h2>");
     $("#playerOneWinLoss").html(
-      "<br><p>Wins: " + wins1 + "  Losses: " + losses1 + "</p>"
+      "<br><p>Wins: " + wins1 + "  Losses: " + loss1 + "</p>"
     );
   } else {
     $("#playerOneName").html("Waiting for Player 1..");
@@ -64,7 +63,7 @@ player2.on("value", function(snapshot) {
     losses = snapshot.val().losses;
     $("#playerTwoName").html("<h2>" + p2 + "</h2>");
     $("#playerTwoWinLoss").html(
-      "<br><p>Wins: " + wins2 + "  Losses: " + losses2 + "</p>"
+      "<br><p>Wins: " + wins2 + "  Losses: " + loss2 + "</p>"
     );
   } else {
     $("#playerTwoName").html("Waiting for Player 2..");
@@ -92,7 +91,7 @@ $("#newPlayer").on("click", function() {
     p2snapshot = snapshot;
   });
   if (!p1snapshot.exists()) {
-    playerNum = 1;
+    playerNumber = 1;
     player1.onDisconnect().remove();
     player1.set({
       player: player,
@@ -104,7 +103,7 @@ $("#newPlayer").on("click", function() {
       $("#gameMessage").html("Waiting for Player 2 to join..");
     }
   } else if (!p2snapshot.exists()) {
-    playerNum = 2;
+    playerNumber = 2;
     player2.onDisconnect().remove();
     player2.set({
       player: player,
@@ -140,63 +139,91 @@ var findResults = function() {
   // Game Section
   if (p1result.val() !== null && p2result.val() !== null) {
     if (p1result.val().choice == p2result.val().choice) {
-      $("#playerOneChoices").html("<h1>" + p1result.val().choice + "</h1>");
-      $("#playerTwoChoices").html("<h1>" + p2result.val().choice + "</h1>");
+      $("#playerOneChoices").html(
+        "<img src='assets/images/" + p1result.val().choice + ".png'>"
+      );
+      $("#playerTwoChoices").html(
+        "<img src='assets/images/" + p2result.val().choice + "2.png'>"
+      );
       $("#gameMessage").html("<h1>Tie Game!</h1>");
     } else if (
       p1result.val().choice == "ROCK" &&
       p2result.val().choice == "SCISSORS"
     ) {
-      $("#playerOneChoices").html("<h1>" + p1result.val().choice + "</h1>");
-      $("#playerTwoChoices").html("<h1>" + p2result.val().choice + "</h1>");
+      $("#playerOneChoices").html(
+        "<img src='assets/images/" + p1result.val().choice + ".png'>"
+      );
+      $("#playerTwoChoices").html(
+        "<img src='assets/images/" + p2result.val().choice + "2.png'>"
+      );
       $("#gameMessage").html("<h1>" + p1 + " wins!</h1>");
       wins1++;
-      losses2++;
+      loss2++;
     } else if (
       p1result.val().choice == "PAPER" &&
       p2result.val().choice == "ROCK"
     ) {
-      $("#playerOneChoices").html("<h1>" + p1result.val().choice + "</h1>");
-      $("#playerTwoChoices").html("<h1>" + p2result.val().choice + "</h1>");
+      $("#playerOneChoices").html(
+        "<img src='assets/images/" + p1result.val().choice + ".png'>"
+      );
+      $("#playerTwoChoices").html(
+        "<img src='assets/images/" + p2result.val().choice + "2.png'>"
+      );
       $("#gameMessage").html("<h1>" + p1 + " wins!</h1>");
       wins1++;
-      losses2++;
+      loss2++;
     } else if (
       p1result.val().choice == "SCISSORS" &&
       p2result.val().choice == "PAPER"
     ) {
-      $("#playerOneChoices").html("<h1>" + p1result.val().choice + "</h1>");
-      $("#playerTwoChoices").html("<h1>" + p2result.val().choice + "</h1>");
+      $("#playerOneChoices").html(
+        "<img src='assets/images/" + p1result.val().choice + ".png'>"
+      );
+      $("#playerTwoChoices").html(
+        "<img src='assets/images/" + p2result.val().choice + "2.png'>"
+      );
       $("#gameMessage").html("<h1>" + p1 + " wins!</h1>");
       wins1++;
-      losses2++;
+      loss2++;
     } else if (
       p1result.val().choice == "ROCK" &&
       p2result.val().choice == "PAPER"
     ) {
-      $("#playerOneChoices").html("<h1>" + p1result.val().choice + "</h1>");
-      $("#playerTwoChoices").html("<h1>" + p2result.val().choice + "</h1>");
+      $("#playerOneChoices").html(
+        "<img src='assets/images/" + p1result.val().choice + ".png'>"
+      );
+      $("#playerTwoChoices").html(
+        "<img src='assets/images/" + p2result.val().choice + "2.png'>"
+      );
       $("#gameMessage").html("<h1>" + p2 + " wins!</h1>");
       wins2++;
-      losses1++;
+      loss1++;
     } else if (
       p1result.val().choice == "PAPER" &&
       p2result.val().choice == "SCISSORS"
     ) {
-      $("#playerOneChoices").html("<h1>" + p1result.val().choice + "</h1>");
-      $("#playerTwoChoices").html("<h1>" + p2result.val().choice + "</h1>");
+      $("#playerOneChoices").html(
+        "<img src='assets/images/" + p1result.val().choice + ".png'>"
+      );
+      $("#playerTwoChoices").html(
+        "<img src='assets/images/" + p2result.val().choice + "2.png'>"
+      );
       $("#gameMessage").html("<h1>" + p2 + " wins!</h1>");
       wins2++;
-      losses1++;
+      loss1++;
     } else if (
       p1result.val().choice == "SCISSORS" &&
       p2result.val().choice == "ROCK"
     ) {
-      $("#playerOneChoices").html("<h1>" + p1result.val().choice + "</h1>");
-      $("#playerTwoChoices").html("<h1>" + p2result.val().choice + "</h1>");
+      $("#playerOneChoices").html(
+        "<img src='assets/images/" + p1result.val().choice + ".png'>"
+      );
+      $("#playerTwoChoices").html(
+        "<img src='assets/images/" + p2result.val().choice + "2.png'>"
+      );
       $("#gameMessage").html("<h1>" + p2 + " wins!</h1>");
       wins2++;
-      losses1++;
+      loss1++;
     }
 
     // Replay Game
@@ -210,7 +237,7 @@ var findResults = function() {
       if (p1result.val() !== null) {
         player1.update({
           wins: wins1,
-          losses: losses1
+          losses: loss1
         });
       }
       player2.once("value", function(snapshot) {
@@ -219,7 +246,7 @@ var findResults = function() {
       if (p2result.val() !== null) {
         player2.update({
           wins: wins2,
-          losses: losses2
+          losses: loss2
         });
       }
       $("#gameMessage").html("");
@@ -231,13 +258,13 @@ var findResults = function() {
 // Database Update
 playerTurn.on("value", function(snapshot) {
   if (snapshot.val() !== null) {
-    if (snapshot.val().turn == 2 && playerNum == 1) {
+    if (snapshot.val().turn == 2 && playerNumber == 1) {
       $("#gameMessage").html("Waiting for " + p2 + " to choose..");
-    } else if (snapshot.val().turn == 1 && playerNum == 2) {
+    } else if (snapshot.val().turn == 1 && playerNumber == 2) {
       $("#playerOneChoices").html("");
       $("#gameMessage").html("Waiting for " + p1 + " to choose..");
     }
-    if (snapshot.val().turn == 1 && playerNum == 1) {
+    if (snapshot.val().turn == 1 && playerNumber == 1) {
       $("#playerOneChoices").empty();
       $("#playerOneChoices").append(
         "<div class='btn-block btn-secondary choice'>ROCK</div>"
@@ -249,7 +276,7 @@ playerTurn.on("value", function(snapshot) {
         "<div class='btn-block btn-secondary choice'>SCISSORS</div>"
       );
       $("#gameMessage").html("- Your Turn -");
-    } else if (snapshot.val().turn == 2 && playerNum == 2) {
+    } else if (snapshot.val().turn == 2 && playerNumber == 2) {
       $("#playerTwoChoices").empty();
       $("#playerTwoChoices").append(
         "<div class='btn-block btn-secondary choice'>ROCK</div>"
@@ -293,3 +320,26 @@ $("#playerTwoChoices").on("click", "div", function() {
     });
   }, 500);
 });
+
+// Chat Functions
+$("#chatButton").on("click", function(event) {
+  event.preventDefault();
+  var text = $("#userChat")
+    .val()
+    .trim();
+  if (text !== "") {
+    $("#userChat").val("");
+    database.ref("/chat/").push({
+      player: player + ": ",
+      chat: text,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+  }
+});
+
+database
+  .ref("/chat")
+  .orderByChild("dateAdded")
+  .on("child_added", function(snapshot) {
+    $("#chatBox").append(snapshot.val().player + snapshot.val().chat + "<br>");
+  });
